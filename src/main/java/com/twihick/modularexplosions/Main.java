9ced0259988ba8b98998d75fd80dbcd3b70cd9a0
Twiguinou <64325771+Twiguinou@users.noreply.github.com>
2020-04-29 08:31:35 +0200

    Such changement

---
+++ src/main/java/com/twihick/modularexplosions/Main.java
@@ -0,0 +1,7 @@
+package com.twihick.modularexplosions;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(StringID.ID)
+public class Main {
+}
---
+++ src/main/java/com/twihick/modularexplosions/StringID.java
@@ -0,0 +1,3 @@
+package com.twihick.modularexplosions;
+
+public final class StringID {public static final String ID = "modularexplosions";}
