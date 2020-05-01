2a906f762b90d13a66a136a216ff83533846e028
Twiguinou <64325771+Twiguinou@users.noreply.github.com>
2020-05-01 10:26:14 +0200

    pls push

--- src/main/java/com/twihick/modularexplosions/client/gui/ConfigurableBombScreen.java
+++ src/main/java/com/twihick/modularexplosions/client/gui/ConfigurableBombScreen.java
@@ -1,10 +1,14 @@
 package com.twihick.modularexplosions.client.gui;
 
+import com.twihick.modularexplosions.StringID;
 import com.twihick.modularexplosions.tileentities.ConfigurableBombTileEntity;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TranslationTextComponent;
 
 public class ConfigurableBombScreen extends Screen {
+
+    private static final ResourceLocation GUI_BACKGROUND = new ResourceLocation(StringID.ID, "textures/gui/configurable_bomb_screen.png");
 
     private final ConfigurableBombTileEntity bomb;
 
