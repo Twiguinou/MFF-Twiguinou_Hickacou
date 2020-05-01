fa837dbefae22d861115ce3ddcc11eedcda75882
Twiguinou <64325771+Twiguinou@users.noreply.github.com>
2020-05-01 13:10:59 +0200

    Configurable Block Added

--- src/main/java/com/twihick/modularexplosions/blocks/ConfigurableBombBlock.java
+++ src/main/java/com/twihick/modularexplosions/blocks/ConfigurableBombBlock.java
@@ -1,14 +1,33 @@
 package com.twihick.modularexplosions.blocks;
 
+import com.twihick.modularexplosions.client.gui.ConfigurableBombScreen;
 import com.twihick.modularexplosions.properties.ExtendedBlockStateProperties;
 import com.twihick.modularexplosions.tileentities.ConfigurableBombTileEntity;
+import com.twihick.modularexplosions.util.world.CustomExplosion;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.item.TNTEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
@@ -23,6 +42,67 @@ public class ConfigurableBombBlock extends AbstractFacingAlignedBlock {
     }
 
     @Override
+    public void catchFire(BlockState state, World world, BlockPos pos, @Nullable net.minecraft.util.Direction face, @Nullable LivingEntity igniter) {
+        explode(world, pos);
+    }
+
+    @Override
+    public void onBlockAdded(BlockState state, World worldIn, BlockPos pos, BlockState oldState, boolean isMoving) {
+        if(oldState.getBlock() != state.getBlock()) {
+            if(worldIn.isBlockPowered(pos)) {
+                catchFire(state, worldIn, pos, null, null);
+                worldIn.removeBlock(pos, false);
+            }
+        }
+    }
+
+    @Override
+    public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
+        if(worldIn.isBlockPowered(pos)) {
+            catchFire(state, worldIn, pos, null, null);
+            worldIn.removeBlock(pos, false);
+        }
+    }
+
+    @Override
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult p_225533_6_) {
+        ItemStack itemstack = player.getHeldItem(handIn);
+        Item item = itemstack.getItem();
+        if(item != Items.FLINT_AND_STEEL && item != Items.FIRE_CHARGE) {
+            Minecraft.getInstance().displayGuiScreen(new ConfigurableBombScreen((ConfigurableBombTileEntity)worldIn.getTileEntity(pos)));
+            return super.onBlockActivated(state, worldIn, pos, player, handIn, p_225533_6_);
+        }else {
+            catchFire(state, worldIn, pos, p_225533_6_.getFace(), player);
+            worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), 11);
+            if(!player.isCreative()) {
+                if(item == Items.FLINT_AND_STEEL) {
+                    itemstack.damageItem(1, player, (p_220287_1_) -> {
+                        p_220287_1_.sendBreakAnimation(handIn);
+                    });
+                }else {
+                    itemstack.shrink(1);
+                }
+            }
+            return ActionResultType.SUCCESS;
+        }
+    }
+
+    public void onBlockHarvested(World worldIn, BlockPos pos, BlockState state, PlayerEntity player) {
+        if(!worldIn.isRemote() && !player.isCreative() && state.get(ACTIVATED)) {
+            catchFire(state, worldIn, pos, null, null);
+        }
+        super.onBlockHarvested(worldIn, pos, state, player);
+    }
+
+    private static void explode(World worldIn, BlockPos pos) {
+        if(!worldIn.isRemote) {
+            ConfigurableBombTileEntity bomb = (ConfigurableBombTileEntity) worldIn.getTileEntity(pos);
+            CustomExplosion explosion = new CustomExplosion(worldIn, pos, bomb.radius_one/2, 0);
+            explosion.explodeExcluding(null, bomb.radius_two/2);
+        }
+    }
+
+    @Override
     public boolean hasTileEntity(BlockState state) {
         return true;
     }
@@ -31,6 +111,19 @@ public class ConfigurableBombBlock extends AbstractFacingAlignedBlock {
     @Override
     public TileEntity createTileEntity(BlockState state, IBlockReader worldIn) {
         return new ConfigurableBombTileEntity();
+    }
+
+    @Override
+    public void onProjectileCollision(World worldIn, BlockState state, BlockRayTraceResult rtr, Entity projectile) {
+        if(!worldIn.isRemote && projectile instanceof AbstractArrowEntity) {
+            AbstractArrowEntity abstractarrowentity = (AbstractArrowEntity) projectile;
+            Entity entity = abstractarrowentity.getShooter();
+            if(abstractarrowentity.isBurning()) {
+                BlockPos blockpos = rtr.getPos();
+                catchFire(state, worldIn, blockpos, null, entity instanceof LivingEntity ? (LivingEntity)entity : null);
+                worldIn.removeBlock(blockpos, false);
+            }
+        }
     }
 
     @Override
--- src/main/java/com/twihick/modularexplosions/client/gui/ConfigurableBombScreen.java
+++ src/main/java/com/twihick/modularexplosions/client/gui/ConfigurableBombScreen.java
@@ -1,12 +1,16 @@
 package com.twihick.modularexplosions.client.gui;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.twihick.modularexplosions.StringID;
 import com.twihick.modularexplosions.tileentities.ConfigurableBombTileEntity;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
+
+import java.awt.*;
 
 public class ConfigurableBombScreen extends Screen {
 
@@ -30,22 +34,37 @@ public class ConfigurableBombScreen extends Screen {
     public void init() {
         int leftCorner = (this.width-this.xSize) / 2;
         int topCorner = (this.height-this.ySize) / 2;
-        this.radiusOnePlus = this.addButton(new Button(leftCorner+10, topCorner+170, 40, 20, I18n.format("gui.button.modularexplosions.radiusOnePlus"), button -> {
+        this.radiusOnePlus = this.addButton(new Button(leftCorner+10, topCorner+165, 80, 20, I18n.format("gui.button.modularexplosions.radiusOnePlus"), button -> {
             this.bomb.setRadiusOne(this.bomb.radius_one+1);
         }));
-        this.radiusOneMinus = this.addButton(new Button(leftCorner+10, topCorner+170, 40, 20, I18n.format("gui.button.modularexplosions.radiusOneMinus"), button -> {
+        this.radiusOneMinus = this.addButton(new Button(leftCorner+10, topCorner+190, 80, 20, I18n.format("gui.button.modularexplosions.radiusOneMinus"), button -> {
             this.bomb.setRadiusOne(this.bomb.radius_one-1);
         }));
-        this.radiusTwoPlus = this.addButton(new Button(leftCorner+10, topCorner+170, 40, 20, I18n.format("gui.button.modularexplosions.radiusOnePlus"), button -> {
+        this.radiusTwoPlus = this.addButton(new Button(leftCorner+130, topCorner+165, 80, 20, I18n.format("gui.button.modularexplosions.radiusTwoPlus"), button -> {
             this.bomb.setRadiusTwo(this.bomb.radius_two+1);
         }));
-        this.radiusTwoMinus = this.addButton(new Button(leftCorner+10, topCorner+170, 40, 20, I18n.format("gui.button.modularexplosions.radiusOnePlus"), button -> {
+        this.radiusTwoMinus = this.addButton(new Button(leftCorner+130, topCorner+190, 80, 20, I18n.format("gui.button.modularexplosions.radiusTwoMinus"), button -> {
             this.bomb.setRadiusTwo(this.bomb.radius_two-1);
         }));
-        this.radiusOnePlus.active = false;
-        this.radiusOneMinus.active = false;
-        this.radiusTwoPlus.active = false;
-        this.radiusTwoMinus.active = false;
+        this.radiusOnePlus.active = true;
+        this.radiusOneMinus.active = true;
+        this.radiusTwoPlus.active = true;
+        this.radiusTwoMinus.active = true;
+    }
+
+    @Override
+    public void render(int mouseX, int mouseY, float partialTicks) {
+        this.renderBackground();
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+        this.minecraft.getTextureManager().bindTexture(GUI_BACKGROUND);
+        int leftCorner = (this.width-this.xSize) / 2;
+        int topCorner = (this.height-this.ySize) / 2;
+        this.blit(leftCorner, topCorner, 0, 0, this.xSize, this.ySize);
+        String WIDTH_TEXT = "Width = " + this.bomb.radius_one;
+        String HEIGHT_TEXT = "Height = " + this.bomb.radius_two;
+        super.render(mouseX, mouseY, partialTicks);
+        this.font.drawString(new StringTextComponent(WIDTH_TEXT).getFormattedText(), leftCorner+110-(font.getStringWidth(WIDTH_TEXT)/2), topCorner+60, Color.WHITE.getRGB());
+        this.font.drawString(new StringTextComponent(HEIGHT_TEXT).getFormattedText(), leftCorner+110-(font.getStringWidth(HEIGHT_TEXT)/2), topCorner+100, Color.WHITE.getRGB());
     }
 
 }
